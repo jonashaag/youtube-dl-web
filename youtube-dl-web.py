19ee618fdaacb006ad9b1fda98330767b27fe394
Jonas Haag <jonas@lophus.org>
2014-11-27 13:42:48 +0100

    More error handling

--- youtube-dl-web.py
+++ youtube-dl-web.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 import json
 from flask import Flask, request, render_template
@@ -6,16 +7,19 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    error = False
+    error = None
     result = None
     url = request.form.get('url')
     if url:
-        try:
-            stdout = subprocess.check_output(["youtube-dl", "-j", url])
-        except subprocess.CalledProcessError:
-            error = True
+        if not re.match("^https?://(www.)?youtube.com/watch\\?v=.+$", url):
+          error = "URL muss mit <tt>https://www.youtube.com/watch?v=</tt> beginnen"
         else:
-            result = json.loads(stdout.decode('utf8'))
+          try:
+              stdout = subprocess.check_output(["youtube-dl", "-j", url])
+          except subprocess.CalledProcessError:
+              error = "Fehler beim Abrufen des Videos :-("
+          else:
+              result = json.loads(stdout.decode('utf8'))
     return render_template("home.html", error=error, result=result)
 
 
